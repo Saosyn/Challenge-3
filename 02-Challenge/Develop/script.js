cb142cd2ca0728d0618dc1195fd3ddcb24a098ce
Timothy Ehli <tehli1882@gmail.com>
2023-09-11 16:00:47 -0600

    fixed password generation from undefinded to display proper output

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,4 +1,4 @@
-var characterLength = 8;
+var charLength = 16;
 var charArr = [];
 
 var specCharArr = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '<', '>', '?', '/', ';', ':'];
@@ -26,33 +26,33 @@ function writePassword() {
 }
 
 function generatePassword() {
-  // console.log("hey! you clicked the button");
   var password = "";
-  for(var i = 0; i < characterLength; i++) {
-    var randomIndex = Math.floor(Math.random() * choiceArr.Length);
-    password = password = choiceArr[randomIndex];
+  for(var i = 0; i < charLength; i++) {
+    var randomIndex = Math.floor(Math.random() * choiceArr.length);
+    password = password + choiceArr[randomIndex];
   }
   return password;
 
 }
 function getPrompts(){
   choiceArr = [];
+
   charLength = parseInt(prompt("How many characters would you like to use? Between 8 and 128."));
 
-  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
-    alert("Character length must be between a number between 8 and 128. Please enter again");
+  if(isNaN(charLength) || charLength < 8 || charLength > 128) {
+    alert("Character length must be a number between 8 and 128. Please enter again");
     return false;
   }
-  if (confirm("would you like lowercase letters?")) {
+  if (confirm("would you like lowercase letters in your password?")) {
     choiceArr = choiceArr.concat(lowerCaseArr);
   }
-  if (confirm("would you like uppercase letters?")) {
+  if (confirm("would you like uppercase letters in your password?")) {
     choiceArr = choiceArr.concat(upperCaseArr);
   }
-  if (confirm("would you like numeric characters?")) {
+  if (confirm("would you like numeric characters in your password?")) {
     choiceArr = choiceArr.concat(numArr);
   }
-  if (confirm("would you like special characters?")) {
+  if (confirm("would you like special characters in your password?")) {
     choiceArr = choiceArr.concat(specCharArr);
   }
   return true;
